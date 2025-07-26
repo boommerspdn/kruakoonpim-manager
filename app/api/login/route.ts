7945a47cd59013f68fa570a2092cf623006bb12d
boommerspdn <131756488+boommerspdn@users.noreply.github.com>
2025-07-26 16:13:21 +0700

    update login cookie behaviour

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -10,12 +10,14 @@ export async function POST(req: Request) {
   ) {
     const res = NextResponse.json({ success: true });
     // Set "infinite" cookie (~10 years)
+    const isDev = process.env.NODE_ENV !== "production";
+
     res.cookies.set("session", "valid", {
       path: "/",
       maxAge: 60 * 60 * 24 * 365 * 10, // 10 years
-      httpOnly: false, // allow JS access
-      secure: false, // allow on HTTP too
-      sameSite: "lax",
+      httpOnly: true,
+      secure: !isDev, // true in prod only
+      sameSite: isDev ? "lax" : "none", // use "Lax" in dev to allow http
     });
 
     return res;
